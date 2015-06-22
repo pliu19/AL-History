a2d65173624efad70856a2cdf1d489fc17d63f1c
pliu19 <pliu19@hawk.iit.edu>
2015-06-22 00:46:25 -0400

    Update get_history.py

--- get_history.py
+++ get_history.py
@@ -3,6 +3,7 @@ import glob
 import numpy as np
 import scipy.sparse as ss
 import argparse
+import os
 from time import time
 import matplotlib as plt
 from sklearn.metrics import accuracy_score
@@ -15,18 +16,18 @@ from strategies import RandomStrategy, UncStrategy, BootstrapFromEach, QBCStrate
 
 class LearningCurve(object):
     """Class - run multiple trials or run trials one at a time"""
-    def run_trials(self, X_pool, y_pool, X_test, y_test, al_strategy, classifier_name, bootstrap_size,  step_size, budget, num_trials):
+    def run_trials(self, X_pool, y_pool, X_test, y_test, al_strategy, classifier_name, bootstrap_size,  step_size, budget, num_trials, path,lengthoftxt):
 
         self.history_probas = { }
         self.history_preditcion = { }
 
         for t in range(num_trials):
             print "trial", t+1
-            self.history_preditcion[t],self.history_probas[t] = self._run_a_single_trial(X_pool, y_pool, X_test, y_test,al_strategy, classifier_name, bootstrap_size,  step_size, budget, t)
+            self._run_a_single_trial(X_pool, y_pool, X_test, y_test,al_strategy, classifier_name, bootstrap_size,  step_size, budget, t,path,lengthoftxt)
 
-        return self.history_preditcion, self.history_probas
 
-    def _run_a_single_trial(self, X_pool, y_pool, X_test, y_test, al_strategy, classifier_name, bootstrap_size,  step_size, budget, t):
+
+    def _run_a_single_trial(self, X_pool, y_pool, X_test, y_test, al_strategy, classifier_name, bootstrap_size,  step_size, budget, t, folderpath,lengthoftxt):
         """Helper method for running multiple trials."""
 
         rows = len(y_test)
@@ -54,7 +55,6 @@ class LearningCurve(object):
             active_s = UncStrategy(seed=t)
 
         model = None
-
         labels = np.unique(y_pool)
 
         #Loop for prediction
@@ -78,9 +78,12 @@ class LearningCurve(object):
             y_pred = model.predict(X_test)
 
             # Save the prediction and probas
-            for num in range(rows):
-                result_prediction[num][ite] = y_pred[num]
-                result_probas[num][ite] = y_probas[num][0]
+            # for num in range(rows):
+            #     result_prediction[num][ite] = y_pred[num]
+            #     result_probas[num][ite] = y_probas[num][0]
+
+            result_prediction[:,ite] = y_pred
+            result_probas[:,ite] = y_probas[:,0]
 
             ite = ite + 1
 
@@ -91,7 +94,15 @@ class LearningCurve(object):
             result_prediction[num][ite] = y_test[num]
             result_probas[num][ite] = y_test[num]
 
-        return result_prediction, result_probas
+        file_name_proba = folderpath + args.strategies + "_" + "Trial" + "_" + str(t+1) + "_proba"
+        np.savetxt("%s.csv" %file_name_proba, result_probas, delimiter=",")
+
+        file_name_prediction = folderpath + args.strategies + "_" + "Trial" + "_" + str(t+1) + "_prediction"
+        np.savetxt("%s.csv" %file_name_prediction, result_prediction, delimiter=",")
+        # crosstable = table_of_cross(result_prediction,lengthoftxt)
+        # file_name_crosstable = folderpath + args.strategies + "_" + "Trial" + "_" + str(t+1) + "_crosstable"
+        # np.savetxt("%s.csv" %file_name_crosstable, crosstable, delimiter=",")
+
 
 def load_imdb(path, shuffle=True, random_state=42, \
               vectorizer = CountVectorizer(min_df=5, max_df=1.0, binary=True)):
@@ -250,8 +261,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-path', help='The path to the content file.')
 
-    parser.add_argument('-classifier',choices=['LogisticRegression()','MultinomialNB()'], default='MultinomialNB',
+    parser.add_argument('-classifier',choices=['LogisticRegression','MultinomialNB'], default='LogisticRegression',
                         help='The underlying classifier.')
+    parser.add_argument("-a","--arguments", default='C=0.1',
+                        help="Represents the arguments that will be passed to the classifier (default: '').")
     parser.add_argument("-nt", "--num_trials", type=int, default=10, help="Number of trials (default: 10).")
 
     parser.add_argument('-num_folds', type=int, default=10, help='The number of folds.')
@@ -259,13 +272,12 @@ if __name__ == '__main__':
     # parser.add_argument("-f", '--file', type=str, default="aaa.aaa",
     #                     help='This feature represents the name that will be written with the result. '
     #                          'If it is left blank, the file will not be written (default: None ).')
-    parser.add_argument("-st", "--strategies", choices=['qbc', 'rand','unc'], nargs='*',default='unc',
+    parser.add_argument("-st", "--strategies", choices=['qbc', 'rand','unc'], nargs='*',default='rand',
                         help="Represent a list of strategies for choosing next samples (default: unc).")
-    parser.add_argument("-a","--arguments", default='',
-                        help="Represents the arguments that will be passed to the classifier (default: '').")
+
     parser.add_argument("-bs", '--bootstrap', default=10, type=int,
                         help='Sets the Boot strap (default: 10).')
-    parser.add_argument("-b", '--budget', default=200, type=int,
+    parser.add_argument("-b", '--budget', default=10000, type=int,
                         help='Sets the budget (default: 2000).')
     parser.add_argument("-sz", '--stepsize', default=10, type=int,
                         help='Sets the step size (default: 10).')
@@ -278,58 +290,37 @@ if __name__ == '__main__':
     lengthoftxt = get_lengthoftext(indices,"C:\\Users\\Ping\\Desktop\\aclImdb")
 
     # Directly use the classifier and calculate the accuracy
-
     combine_classifier = get_classifier(args.classifier, args.arguments)
     model = eval(combine_classifier)
     model.fit(X_tr,y_tr)
     direct = model.predict(X_te)
     directaccuracy = accuracy_score(y_te,direct)
-    print "directly use the classifier, the accuracy is %r" %directaccuracy
+    print "Directly use the classifier, the accuracy is %r" %directaccuracy
 
     # Use the Active Learning
+    folderpath = './' +args.strategies + '_' + args.classifier + '_' + args.arguments + '/'
+    os.mkdir(folderpath)
+
     learning_api = LearningCurve()
-    his_predition, his_probal = learning_api.run_trials(X_tr, y_tr, X_te, y_te, args.strategies, combine_classifier, args.bootstrap, args.stepsize, args.budget, args.num_trials)
+    learning_api.run_trials(X_tr, y_tr, X_te, y_te, args.strategies, combine_classifier, args.bootstrap, args.stepsize, args.budget, args.num_trials,folderpath,lengthoftxt)
 
-    for i in his_probal.keys():
-        file_name_proba = args.strategies + "_" + "Trial" + "_" + str(i+1) + "_proba"
-        np.savetxt("%s.csv" %file_name_proba, his_probal[i], delimiter=",")
+    # column_file = (args.budget - args.bootstrap) / args.stepsize + 2
+    # row_file = len(y_te)
+    # mean_probal = np.zeros(shape=(row_file,column_file))
+    # for i in range(args.num_trials):
 
-    for i in his_predition.keys():
-        file_name_prediction = args.strategies + "_" + "Trial" + "_" + str(i+1) + "_prediction"
-        np.savetxt("%s.csv" %file_name_prediction, his_predition[i], delimiter=",")
-        crosstable = table_of_cross(his_predition[i],lengthoftxt)
-        file_name_crosstable = args.strategies + "_" + "Trial" + "_" + str(i+1) + "_crosstable"
-        np.savetxt("%s.csv" %file_name_crosstable, crosstable, delimiter=",")
-
-    # Get the max, min of probability in the 10 trials
-
-    row, column = his_probal[0].shape
-    # max_probal = np.zeros(shape=(row,column))
-    # min_probal = np.ones(shape=(row,column))
-    #
-    # for i in his_probal.keys():
-    #     current = his_probal[i]
-    #     for a in range(row):
-    #         for b in range(column):
-    #             if max_probal[a][b] < current[a][b]:
-    #                 max_probal[a][b] = current[a][b]
-    #
-    # for i in his_probal.keys():
-    #     current = his_probal[i]
-    #     for a in range(row):
-    #         for b in range(column):
-    #             if min_probal[a][b] > current[a][b]:
-    #                 min_probal[a][b] = current[a][b]
-
+#################################################################################
     # Get the mean of probability and cross table
-    mean_probal = np.zeros(shape=(row, column))
-    for i in his_probal.keys():
-        mean_probal += his_probal[i]
-    mean_probal = mean_probal/10
-    file_name_mean_prediction = args.strategies + "_" + "Mean" + "_prediction"
-    np.savetxt("%s.csv" %file_name_mean_prediction, mean_probal, delimiter=",")
-    file_name_mean_crosstable = args.strategies + "_Mean" + "_crosstable"
-    np.savetxt("%s.txt" %file_name_mean_crosstable, crosstable.astype(int), delimiter=",",fmt='%i')
+    # row, column = his_probal[0].shape
+    # mean_probal = np.zeros(shape=(row, column))
+    # for i in his_probal.keys():
+    #     mean_probal += his_probal[i]
+    #
+    # mean_probal = mean_probal/10.
+    # file_name_mean_prediction = args.strategies + "_" + "Mean" + "_prediction"
+    # np.savetxt("%s.csv" %file_name_mean_prediction, mean_probal, delimiter=",")
+    # file_name_mean_crosstable = args.strategies + "_Mean" + "_crosstable"
+    # np.savetxt("%s.txt" %file_name_mean_crosstable, crosstable.astype(int), delimiter=",",fmt='%i')
 
 
 
